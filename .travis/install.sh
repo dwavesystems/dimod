ad05c7be3948c2be95422f7b58186c464133743b
oneklc <oneklc@gmail.com>
2017-09-26 16:12:04 -0700

    fixes for mac builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,17 +3,20 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
+    brew install pyenv-virtualenv
 
     case "${TOXENV}" in
         py27)
             # Install some custom Python 2.7 requirements on OS X
+                pip install requirements.txt
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
+                pip install requirements.txt
             ;;
         pynightly)
             # Install some custom Python nightly requirements on OS X
+                pip install requirements.txt
             ;;
 
     esac
