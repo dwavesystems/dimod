60ff57c4e626767752e82c34e07c043c4b03eb92
Alexander Condello <acondello@dwavesys.com>
2017-10-02 08:48:37 -0700

    removing structure

--- dimod/sampler_template.py
+++ dimod/sampler_template.py
@@ -6,16 +6,14 @@ Methods:
 
 - sample_ising
 - sample_qubo
-- sample_structured_ising
-- sample_structured_qubo
 
 Property:
 
 - structure
 
-For many samplers, the `structure` property will be None and the
-structured and unstructured variants of the methods will do the
-same thing. See Structured Samplers section below.
+Some samplers will only function on binary quadratic models with specific
+structures. These samplers should have information in the `structure`
+property. See Structured Samplers section below.
 
 
 Examples
@@ -57,43 +55,13 @@ Also, by implementing one of the methods, we now can use the others.
 >>> list(response.samples())
 [{0: 1, 1: -1}]
 
-Similarly for the structured methods.
-
->>> h = {0: -1, 1: 2}
->>> J = {}
->>> response = MyLinearSampler().sample_structured_ising(h, J)
->>> list(response.samples())
-[{0: 1, 1: -1}]
->>> Q = {(0, 0): 1, (1, 1): 0}
->>> response = MyLinearSampler().sample_structured_qubo(Q)
->>> list(response.samples())
-[{0: 0, 1: 1}]
-
-However, if we assign a structure to our sampler, the structured
-methods will no longer work.
-
->>> sampler = MyLinearSampler()
->>> sampler.structure = 'linear'
->>> try:
-...     sampler.sample_structured_qubo({})
-... except RecursionError:  # infinite recursion
-...     print('not implemented')  # python3
-... except RuntimeError:
-...     print('not implemented')  # python2
-'not implemented'
 
 Structured Samplers
 -------------------
 
 Some samplers can only operate on a particular problem structure.
 Most commonly this happens when there is a particular problem graph.
-In this case, `sample_structured_ising` and `sample_sturctured_qubo`
-would require the user to provide an Ising problem or QUBO of the
-appropriate structure. `sample_ising` and `sample_qubo` could either
-fail when the structure is not provided, but more commonly would
-make an attempt to change the structure to fit. These sorts of
-embedding techniques are discussed here:
-https://www.dwavesys.com/sites/default/files/Map%20Coloring%20WP2.pdf
+In this case, the `structure` property will not be None.
 
 """
 # we could do a solver that requires a complete graph as an example above...
@@ -181,74 +149,6 @@ class TemplateSampler(object):
         Q, offset = ising_to_qubo(h, J)
         binary_response = self.sample_qubo(Q, **kwargs)
         return binary_response.as_spin(offset)
-
-    @qubo(1)
-    def sample_structured_qubo(self, Q, **kwargs):
-        """If sampler is unstructured, invokes the sample_qubo method, otherwise
-        converts the problem to an Ising problem and invokes the
-        sample_structured_ising method.
-
-        See sample_qubo and sample_structured_ising documentation for more
-        information.
-
-        Args:
-            Q (dict): A dictionary defining the QUBO. Should be of the form
-                {(u, v): bias} where u, v are variables and bias is numeric.
-            **kwargs: Any keyword arguments are passed directly to
-                sample_ising.
-
-        Returns:
-            :obj:`BinaryResponse`:
-                A `BinaryResponse`, converted from the `SpinResponse` return
-                from sample_ising.
-
-        Note:
-            This method is inherited from the :obj:`TemplateSampler` base class.
-
-        """
-        if self.structure is not None:
-            h, J, offset = qubo_to_ising(Q)
-            spin_response = self.sample_structured_ising(h, J, **kwargs)
-            return spin_response.as_binary(offset)
-        return self.sample_qubo(Q, **kwargs)
-
-    @ising(1, 2)
-    def sample_structured_ising(self, h, J, **kwargs):
-        """If sampler is unstructured, invokes the sample_ising method, otherwise
-        converts the problem to a QUBO and invokes the sample_structured_qubo
-        method.
-
-        See sample_ising and sample_structured_qubo documentation for more
-        information.
-
-        Args:
-            h (dict/list): The linear terms in the Ising problem. If a
-                dict, should be of the form {v: bias, ...} where v is
-                a variable in the Ising problem, and bias is the linear
-                bias associated with v. If a list, should be of the form
-                [bias, ...] where the indices of the biases are the
-                variables in the Ising problem.
-            J (dict): A dictionary of the quadratic terms in the Ising
-                problem. Should be of the form {(u, v): bias} where u,
-                v are variables in the Ising problem and bias is the
-                quadratic bias associated with u, v.
-            **kwargs: Any keyword arguments are passed directly to
-                sample_qubo.
-
-        Returns:
-            :obj:`SpinResponse`:
-                A `SpinResponse`, converted from the `BinaryResponse`
-                return from sample_ising.
-
-        Note:
-            This method is inherited from the :obj:`TemplateSampler` base class.
-
-        """
-        if self.structure is not None:
-            Q, offset = ising_to_qubo(h, J)
-            binary_response = self.sample_structured_qubo(Q, **kwargs)
-            return binary_response.as_spin(offset)
-        return self.sample_ising(h, J, **kwargs)
 
     @property
     def structure(self):
