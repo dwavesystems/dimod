3c72842733705252959996ad7c4317eaa18d5324
Alexander Condello <acondello@dwavesys.com>
2017-12-22 10:05:03 -0800

    Update setup script to not need requirements

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -2,11 +2,6 @@ from __future__ import absolute_import
 
 import sys
 
-__version__ = '0.4.0'
-__author__ = 'D-Wave Systems Inc.'
-__authoremail__ = 'acondello@dwavesys.com'
-__description__ = 'A shared API for binary quadratic model samplers.'
-
 _PY2 = sys.version_info[0] == 2
 
 from dimod.samplers import *
