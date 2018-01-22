88610ede4f73c4c4c5543218904f4867fe530e26
Alexander Condello <acondello@dwavesys.com>
2018-01-22 14:49:48 -0800

    Update verion 1.0.0.dev2 -> 1.0.0.dev3

--- dimod/package_info.py
+++ dimod/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0.dev2'
+__version__ = '1.0.0.dev3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'A shared API for binary quadratic model samplers.'
