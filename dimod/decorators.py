45098f8371f48696f0a61b8c03b088ff474c9ac2
Alexander Condello <acondello@dwavesys.com>
2018-02-05 13:08:33 -0800

    Refactor Response's change_vartype

--- dimod/decorators.py
+++ dimod/decorators.py
@@ -2,6 +2,7 @@
 
 """
 from dimod.compatibility23 import iteritems
+from dimod.vartypes import Vartype
 
 
 def bqm_index_labels(f):
@@ -46,3 +47,34 @@ def patch_sample_kwargs(f):
             return f(sampler, *args, **kwargs)
 
     return _patch_kwargs
+
+
+def vartype_argument(arg_idx):
+    """todo"""
+    def _vartype_arg(f):
+        def new_f(*args, **kwargs):
+
+            vartype = args[arg_idx]
+
+            if isinstance(vartype, Vartype):
+                # we don't need to do anything
+                return f(*args, **kwargs)
+
+            try:
+                if isinstance(vartype, str):
+                    vartype = Vartype[vartype]
+                else:
+                    vartype = Vartype(vartype)
+
+            except (ValueError, KeyError):
+                raise TypeError(("expected input vartype to be one of: "
+                                 "Vartype.SPIN, 'SPIN', {-1, 1}, "
+                                 "Vartype.BINARY, 'BINARY', or {0, 1}."))
+
+            new_args = list(args)
+            new_args[arg_idx] = vartype
+
+            return f(*new_args, **kwargs)
+        new_f.__name__ = f.__name__
+        return new_f
+    return _vartype_arg
