78cf5096dc5cfbc6371007bcc4709ebb1d5e3537
Alexander Condello <acondello@dwavesys.com>
2018-02-07 08:26:45 -0800

    Update setup.py to reflect new project structure.
    * Change __version__ import to allow it to be run from anywhere
    * Add README as long description

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 from __future__ import absolute_import
 
 import sys
+import os
+
 from setuptools import setup
 
-_PY2 = sys.version_info.major == 2
-
 # add __version__, __author__, __authoremail__, __description__ to this namespace
-# equivalent to:
+_PY2 = sys.version_info.major == 2
+my_loc = os.path.dirname(os.path.abspath(__file__))
+os.chdir(my_loc)
 if _PY2:
-    execfile("./dimod/package_info.py")
+    execfile(os.path.join(".", "dimod", "package_info.py"))
 else:
-    exec(open("./dimod/package_info.py").read())
+    exec(open(os.path.join(".", "dimod", "package_info.py")).read())
 
-install_requires = ['enum34>=1.1.6']
+install_requires = ['enum34>=1.1.6<2.0.0',
+                    'numpy>=1.14.0<2.0.0',
+                    'pandas>=0.22.0<0.23.0']
 
 extras_require = {'docs': ['sphinx', 'sphinx_rtd_theme', 'recommonmark'],
-                  'all': ['numpy>=1.14.0', 'pandas>=0.22.0', 'networkx>=2.0']}
+                  'all': ['networkx>=2.0<3.0']}
 
 packages = ['dimod',
-            'dimod.response',
-            'dimod.composites',
-            'dimod.samplers',
-            'dimod.binary_quadratic_model']
+            'dimod.classes',
+            'dimod.reference.composites',
+            'dimod.reference.samplers',
+            'dimod.test']
 
 setup(
     name='dimod',
@@ -29,8 +33,9 @@ setup(
     author=__author__,
     author_email=__authoremail__,
     description=__description__,
+    long_description=open('README.rst').read(),
     url='https://github.com/dwavesystems/dimod',
-    download_url='https://github.com/dwavesys/dimod/archive/0.1.1.tar.gz',
+    download_url='https://github.com/dwavesystems/dimod/releases',
     license='Apache 2.0',
     packages=packages,
     install_requires=install_requires,
