07abb116f4adfa2cba2f322135b5db2506582c54
Alexander Condello <acondello@dwavesys.com>
2018-02-09 10:12:55 -0800

    Add Structured base class

---
+++ tests/test_structured.py
@@ -0,0 +1,32 @@
+import unittest
+
+import dimod
+
+#
+# docstring tests
+# test that the structured sampler as described in the docstring works appropriately. This will also
+# be caught in doctests but here we can test it more generally
+#
+
+
+class TwoVariables(dimod.Sampler, dimod.Structured):
+    def __init__(self):
+        dimod.Sampler.__init__(self)
+        dimod.Structured.__init__(self, [0, 1], [(0, 1)])
+
+    @dimod.decorators.bqm_structured
+    def sample(self, bqm):
+        # All bqm's passed in will be a subgraph of the sampler's structure
+        variable_list = list(bqm.linear)
+        response = dimod.Response(bqm.vartype)
+        for values in itertools.product(bqm.vartype.value, len(bqm)):
+            sample = dict(zip(variable_list, values))
+            energy = bqm.energy(sample)
+            response.add_sample(sample, energy)
+        return response
+
+
+class TestTwoVariables(unittest.TestCase, dimod.test.SamplerAPITest):
+    def setUp(self):
+        self.sampler = TwoVariables()
+        self.sampler_factory = TwoVariables
