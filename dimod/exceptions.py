4599cf55f33d0f551f7a293a028424d095d3ec70
Alexander Condello <acondello@dwavesys.com>
2018-02-09 10:11:50 -0800

    Add exceptions that mark invalid bqm

--- dimod/exceptions.py
+++ dimod/exceptions.py
@@ -8,3 +8,15 @@ class MappingError(Exception):
 
 class InvalidSampler(Exception):
     """Raised when trying to use Sampler as a sampler"""
+
+
+class BinaryQuadraticModelValueError(ValueError):
+    """Raised when a sampler cannot handle a specific bqm"""
+
+
+class BinaryQuadraticModelSizeError(BinaryQuadraticModelValueError):
+    """Raised when the bqm has too many variables"""
+
+
+class BinaryQuadraticModelStructureError(BinaryQuadraticModelValueError):
+    """Raised when the BQM does not fit the sampler"""
