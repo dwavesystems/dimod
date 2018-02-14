86cd3e39747cf26065fded1faf8a8d86875932d7
Alexander Condello <acondello@dwavesys.com>
2018-02-14 07:47:58 -0800

    Update install and deployment

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup
 
 from dwave_embedding_utilities import __version__, __author__, __description__, __authoremail__
 
-tests_require = ['dimod',
-                 'dwave_networkx',
-                 'networkx']
 
 setup(
     name='dwave_embedding_utilities',
@@ -14,6 +11,5 @@ setup(
     description=__description__,
     url='https://github.com/dwavesystems/dwave_embedding_utilities',
     license='Apache 2.0',
-    tests_require=tests_require,
     py_modules=['dwave_embedding_utilities']
 )
