5b3d38821517f10f9b9da31f28af19e7302de954
Alexander Condello <acondello@dwavesys.com>
2018-02-27 14:37:33 -0800

    Update Structure composite to use the new abc

--- dimod/reference/composites/structure.py
+++ dimod/reference/composites/structure.py
@@ -6,13 +6,25 @@ from dimod.decorators import bqm_structured
 
 class StructureComposite(Sampler, Composite, Structured):
     """Creates a structured composed sampler from an unstructured sampler.
-
-    todo
     """
+    # we will override these in the __init__, but because they are abstract properties we need to
+    # signal that we are overriding them
+    edgelist = None
+    nodelist = None
+    children = None
+
     def __init__(self, sampler, nodelist, edgelist):
-        Sampler.__init__(self)
-        Composite.__init__(self, sampler)
-        Structured.__init__(self, nodelist, edgelist)
+        self.children = [sampler]
+        self.nodelist = nodelist
+        self.edgelist = edgelist
+
+    @property
+    def parameters(self):
+        return self.child.parameters
+
+    @property
+    def properties(self):
+        return self.child.properties
 
     @bqm_structured
     def sample(self, bqm, **sample_kwargs):
