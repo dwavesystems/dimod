08363290b1b29946a9f7b0759e41707cee68783c
Alexander Condello <acondello@dwavesys.com>
2018-06-25 09:51:03 -0700

    Add apache 2.0 headers

--- dimod/vartypes.py
+++ dimod/vartypes.py
@@ -1,3 +1,18 @@
+# Copyright 2018 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# ================================================================================================
 """
 Enumeration of valid variable types for binary quadratic models.
 
@@ -49,6 +64,7 @@ class Vartype(enum.Enum):
     """
     SPIN = frozenset({-1, 1})
     BINARY = frozenset({0, 1})
+
 
 SPIN = Vartype.SPIN
 BINARY = Vartype.BINARY
