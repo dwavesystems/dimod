d07fa06b279436984686d36ddb0ae8648cd844fb
Alexander Condello <acondello@dwavesys.com>
2018-09-12 16:00:52 -0700

    Add citations for fix_variables function

--- dimod/roof_duality/fix_variables.py
+++ dimod/roof_duality/fix_variables.py
@@ -3,18 +3,19 @@ from dimod.vartypes import Vartype
 
 
 def fix_variables(bqm, sampling_mode=True):
-    """Determine assignments for some bqm variables.
+    """Determine assignments for some binary quadratic model variables.
 
     fix_variables uses maximum flow in the implication network to correctly fix variables (that is,
     one can find an assignment for the other variables that attains the optimal value). The
-    variables that roof duality fixes will take the same values in all optimal solutions.
+    variables that roof duality fixes will take the same values in all optimal solutions [BHT]_ [BH]_.
 
     Args:
         bqm (:obj:`.BinaryQuadraticModel`)
             A binary quadratic model.
 
         sampling_mode (bool, optional, default=True):
-            Setting sampling_model to False can fix more variables, but in some optimal solutions
+            In sampling mode, only roof-duality is used. When sampling_mode is false, strongly
+            connected components are used to fix more variables, but in some optimal solutions
             these variables may take different values.
 
     Returns:
@@ -27,19 +28,25 @@ def fix_variables(bqm, sampling_mode=True):
         >>> dimod.fix_variables(bqm)
         {'a': -1}
 
-        There are two ground states, so no variables fixed.
+        In this example, there are two ground states, so no variables fixed.
 
         >>> bqm = dimod.BinaryQuadraticModel.empty(dimod.SPIN)
         >>> bqm.add_interaction('a', 'b', -1.0)
         >>> dimod.fix_variables(bqm)
         {}
 
-        With sampling_model off, additional variables are fixed.
+        In this example with sampling_model off, additional variables are fixed.
 
         >>> bqm = dimod.BinaryQuadraticModel.empty(dimod.SPIN)
         >>> bqm.add_interaction('a', 'b', -1.0)
         >>> dimod.fix_variables(bqm, sampling_mode=False) # doctest: +SKIP
         {'a': 1, 'b': 1}
+
+    .. [BHT] Boros, E., P.L. Hammer, G. Tavares. Preprocessing of Unconstraint Quadratic Binary
+        Optimization. Rutcor Research Report 10-2006, April, 2006.
+
+    .. [BH] Boros, E., P.L. Hammer. Pseudo-Boolean optimization. Discrete Applied Mathematics 123,
+        (2002), pp. 155-225
 
     """
     if sampling_mode:
