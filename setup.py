dc4a1e6034bb7c0988eb40c86cc11f03c5be6a29
Alexander Condello <acondello@dwavesys.com>
2018-09-28 11:05:21 -0700

    Remove unnessary win py2.6 support

--- setup.py
+++ setup.py
@@ -114,15 +114,6 @@ if USE_CYTHON:
 # SOFTWARE.
 #
 
-if sys.platform == 'win32' and sys.version_info < (2, 7):
-    # 2.6's distutils.msvc9compiler can raise an IOError when failing to
-    # find the compiler
-    # It can also raise ValueError https://bugs.python.org/issue7511
-    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
-                  IOError, ValueError)
-else:
-    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
-
 
 class BuildFailed(Exception):
     pass
@@ -140,7 +131,7 @@ class ve_build_ext(build_ext):
     def build_extension(self, ext):
         try:
             build_ext.build_extension(self, ext)
-        except ext_errors:
+        except (CCompilerError, DistutilsExecError, DistutilsPlatformError):
             raise BuildFailed()
 
 
