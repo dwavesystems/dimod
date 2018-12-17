60a859ba5895731bd88dfc72147a010077a29f51
Alexander Condello <acondello@dwavesys.com>
2018-12-17 14:21:32 -0800

    Store BQM biases in a single data structure
    We now used views for bqm.linear, bqm.adjacency, bqm.quadratic.
    This way we can keep all the biases in a single bqm._adj dict-of-
    dicts.
    
    We also added functionality for keeping bias and interaction data.

--- dimod/views.py
+++ dimod/views.py
@@ -18,6 +18,8 @@ try:
 except ImportError:
     import collections as abc
 
+from collections import OrderedDict
+
 from six.moves import zip
 
 
@@ -72,3 +74,204 @@ class IndexValuesView(abc.ValuesView):
     def __iter__(self):
         # Inherited __init__ puts the Mapping into self._mapping
         return iter(self._mapping._data.flat)
+
+class Data(object):
+    __slots__ = 'bias', '_data'
+
+    def __init__(self, bias, data=None):
+        self.bias = bias
+
+        if data is not None:
+            self._data = data
+
+    def __repr__(self):
+        if hasattr(self, '_data'):
+            return '{}({!r}, data={!r})'.format(self.__class__.__name__, self.bias, self.data)
+        else:
+            return '{}({!r})'.format(self.__class__.__name__, self.bias)
+
+    def copy(self):
+        if hasattr(self, '_data'):
+            return type(self)(self.bias, self.data.copy())
+        else:
+            return type(self)(self.bias)
+
+    @property
+    def data(self):
+        try:
+            return self._data
+        except AttributeError:
+            pass
+
+        self._data = data = {}
+        return data
+    
+
+class LinearView(abc.MutableMapping):
+    __slots__ = '_adj',
+
+    def __init__(self, bqm):
+        self._adj = bqm._adj
+
+    def __delitem__(self, v):
+        if v not in self:
+            raise KeyError
+        if len(self._adj[v]) > 1:
+            raise ValueError("there are interactions associated with {} that must be deleted first".format(v))
+        del self._adj[v]
+
+    def __getitem__(self, v):
+        return self._adj[v][v].bias
+
+    def __iter__(self):
+        return iter(self._adj)
+
+    def __len__(self):
+        return len(self._adj)
+
+    def __setitem__(self, v, bias):
+        adj = self._adj
+        if v in adj:
+            adj[v][v].bias = bias
+        else:
+            adj[v] = {v: Data(bias)}
+
+    def __str__(self):
+        return str(dict(self))
+
+    def items(self):
+        return LinearItemsView(self)
+
+class OrderedLinearView(LinearView):
+    def __setitem__(self, v, bias):
+        adj = self._adj
+        if v in adj:
+            adj[v][v].bias = bias
+        else:
+            adj[v] = OrderedDict(v=Data(bias))
+
+
+class LinearItemsView(abc.ItemsView):
+    """Faster items iteration"""
+    __slots__ = ()
+
+    def __iter__(self):
+        for v, neighbours in self._mapping._adj.items():
+            yield v, neighbours[v].bias
+
+
+class QuadraticView(abc.MutableMapping):
+    __slots__ = '_adj',
+
+    def __init__(self, bqm):
+        self._adj = bqm._adj
+
+    def __delitem__(self, interaction):
+        u, v = interaction
+        if u == v:
+            raise KeyError
+        adj = self._adj
+        del adj[v][u]
+        del adj[u][v]
+
+    def __getitem__(self, interaction):
+        u, v = interaction
+        if u == v:
+            raise KeyError
+        return self._adj[u][v].bias
+
+    def __iter__(self):
+        seen = set()
+        adj = self._adj
+        for u, neigh in adj.items():
+
+            for v in neigh:
+                if u == v:
+                    # not adjacent to itself
+                    continue
+
+                if v not in seen:
+                    yield (u, v)
+
+            seen.add(u)
+
+    def __len__(self):
+        return sum(len(neighbours) - 1 for neighbours in self._adj.values()) // 2
+
+    def __setitem__(self, interaction, bias):
+        u, v = interaction
+        if u == v:
+            raise KeyError
+
+        adj = self._adj
+        
+        if u not in adj:
+            raise NotImplementedError
+        if v not in adj:
+            raise NotImplementedError
+
+        adj[u][v] = adj[v][u] = Data(bias)
+
+    def __str__(self):
+        return str(dict(self))
+
+    def items(self):
+        return QuadraticItemsView(self)
+
+
+class QuadraticItemsView(abc.ItemsView):
+    """Faster items iteration"""
+    __slots__ = ()
+
+    def __iter__(self):
+        adj = self._mapping._adj
+        for u, v in self._mapping:
+            yield (u, v), adj[u][v].bias
+
+class NeighbourView(abc.Mapping):
+    __slots__ = '_adj', '_var'
+
+    def __init__(self, adj, v):
+        self._adj = adj
+        self._var = v
+
+    def __getitem__(self, v):
+        u = self._var
+        if u == v:
+            raise KeyError
+        return self._adj[u][v].bias
+
+    def __setitem__(self, u, bias):
+        v = self._var
+        if u == v:
+            raise KeyError
+        self._adj[u][v].bias = bias
+
+    def __iter__(self):
+        v = self._var
+        for u in self._adj[v]:
+            if u != v:
+                yield u
+
+    def __len__(self):
+        return len(self._adj[self._var]) - 1  # ignore self
+
+    def __str__(self):
+        return str(dict(self))
+
+class AdjacencyView(abc.Mapping):
+    __slots__ = '_adj',
+
+    def __init__(self, bqm):
+        self._adj = bqm._adj
+
+    def __getitem__(self, v):
+        if v not in self._adj:
+            raise KeyError
+        return NeighbourView(self._adj, v)
+
+    def __iter__(self):
+        return iter(self._adj)
+
+    def __len__(self):
+        return len(self._adj)
