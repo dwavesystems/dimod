c15400a43fb8819c397f5d1b42bbfe505d16c4f3
Alexander Condello <arcondello@gmail.com>
2019-02-08 17:06:51 -0800

    Require numpy 1.15 and support 1.16

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if _PY2:
 else:
     exec(open(os.path.join(".", "dimod", "package_info.py")).read())
 
-install_requires = ['numpy>=1.14.0,<2.0.0',
+install_requires = ['numpy>=1.15.0,<2.0.0',
                     'six>=1.10.0,<2.0.0',
                     'jsonschema>=2.6.0,<3.0.0']
 
