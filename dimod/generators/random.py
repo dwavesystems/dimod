ceee47e049c2c3305d459c6ae865a430dbd113e9
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2019-04-26 05:47:53 -0700

    Fix length of quadratic

--- dimod/generators/random.py
+++ dimod/generators/random.py
@@ -197,7 +197,7 @@ def ran_r(r, graph, cls=BinaryQuadraticModel, seed=None):
     rvals = np.empty(2*r)
     rvals[0:r] = range(-r, 0)
     rvals[r:] = range(1, r+1)
-    qdata = rnd.choice(rvals, size=len(variables))
+    qdata = rnd.choice(rvals, size=len(irow))
 
     offset = 0
 
