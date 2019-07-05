8e45366cc263070d3b391a658c1467b48256315d
Alexander Condello <arcondello@gmail.com>
2019-07-05 15:12:55 -0700

    Rework AdjArrayBQM construction

--- tests/test_cybqm.py
+++ tests/test_cybqm.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from dimod.bqm.cybqm import AdjArrayBQM
 
 
@@ -7,21 +9,43 @@ class TestConstruction(unittest.TestCase):
     """Tests for properties and special methods"""
 
     def test_empty(self):
-        bqm = AdjArrayBQM(0, 0)
+        bqm = AdjArrayBQM()
 
         self.assertEqual(len(bqm), 0)
         self.assertEqual(bqm.shape, (0, 0))
 
-    def test_empty_large_interactions(self):
-        # in this case we've allocated 10000 but because nothing has been set
-        # we still count as having 0 biases
-        bqm = AdjArrayBQM(0, 10000)
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [])
+        self.assertEqual(quad, [])
 
-        self.assertEqual(len(bqm), 0)
-        self.assertEqual(bqm.shape, (0, 0))
-
-    def test_linear_only_large_interactions(self):
-        bqm = AdjArrayBQM(1000, 10000)
+    def test_integral_nonzero(self):
+        bqm = AdjArrayBQM(1000)
 
         self.assertEqual(len(bqm), 1000)
         self.assertEqual(bqm.shape, (1000, 0))
+
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(0, 0) for _ in range(1000)])
+        self.assertEqual(quad, [])
+
+    def test_dense_triu(self):
+        bqm = AdjArrayBQM(np.triu(np.ones((5, 5))))
+
+        self.assertEqual(bqm.shape, (5, 10))
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(d, 1) for d in range(0, 5*4, 4)])
+        self.assertEqual(quad, [(v, 1)
+                                for u in range(5)
+                                for v in range(5)
+                                if u != v])
+
+    def test_dense(self):
+        bqm = AdjArrayBQM([[.1, 1, 2], [0, 0, 0], [1, 1, 0]])
+
+        self.assertEqual(bqm.shape, (3, 3))
+
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(0, .1), (2, 0), (4, 0)])
+        self.assertEqual(quad, [(1, 1), (2, 3),
+                                (0, 1), (2, 1),
+                                (0, 3), (1, 1)])
