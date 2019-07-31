011d5aa672b4e55361b7eee02ef2cb19bba46363
Alexander Condello <arcondello@gmail.com>
2019-07-31 11:51:34 -0700

    Convert to using c++ under the hood for adjbqms

---
+++ dimod/bqm/cppbqm.pxd
@@ -0,0 +1,55 @@
+# distutils: language = c++
+# cython: language_level=3
+#
+# Copyright 2019 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# =============================================================================
+
+from libcpp cimport bool
+from libcpp.map cimport map as cppmap
+from libcpp.pair cimport pair
+from libcpp.vector cimport vector
+
+
+cdef extern from "adjarray.cc":
+    pass
+
+cdef extern from "adjarray.h" namespace "dimod" nogil:
+
+    size_t num_variables[V, B](vector[pair[size_t, B]]&, vector[pair[V, B]]&)
+    size_t num_interactions[V, B](vector[pair[size_t, B]]&, vector[pair[V, B]]&)
+
+
+cdef extern from "adjmap.cc":
+    pass
+
+cdef extern from "adjmap.h" namespace "dimod" nogil:
+
+    size_t num_variables[V, B](vector[pair[cppmap[V, B], B]]&)
+    size_t num_interactions[V, B](vector[pair[cppmap[V, B], B]]&)  
+
+    V add_variable[V, B](vector[pair[cppmap[V, B], B]]&)
+
+    bool add_interaction[V, B](vector[pair[cppmap[V, B], B]]&, V, V)
+
+    V pop_variable[V, B](vector[pair[cppmap[V, B], B]]&)
+
+
+    B get_linear[V, B](vector[pair[cppmap[V, B], B]]&, V)
+    B get_quadratic[V, B](vector[pair[cppmap[V, B], B]]&, V, V)
+
+    void set_linear[V, B](vector[pair[cppmap[V, B], B]]&, V, B)
+
+    void set_quadratic[V, B](vector[pair[cppmap[V, B], B]]&, V, V, B)
---
+++ tests/test_adjarraybqm.py
@@ -0,0 +1,103 @@
+# Copyright 2019 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# =============================================================================
+import itertools
+import unittest
+
+import numpy as np
+
+from dimod.bqm.adjarraybqm import AdjArrayBQM
+
+
+class TestConstruction(unittest.TestCase):
+    """Tests for properties and special methods"""
+
+    def test_empty(self):
+        bqm = AdjArrayBQM()
+
+        self.assertEqual(len(bqm), 0)
+        self.assertEqual(bqm.shape, (0, 0))
+
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [])
+        self.assertEqual(quad, [])
+
+    def test_integral_nonzero(self):
+        bqm = AdjArrayBQM(1000)
+
+        self.assertEqual(len(bqm), 1000)
+        self.assertEqual(bqm.shape, (1000, 0))
+
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(0, 0) for _ in range(1000)])
+        self.assertEqual(quad, [])
+
+    def test_dense_triu(self):
+        bqm = AdjArrayBQM(np.triu(np.ones((5, 5))))
+
+        self.assertEqual(bqm.shape, (5, 10))
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(d, 1) for d in range(0, 5*4, 4)])
+        self.assertEqual(quad, [(v, 1)
+                                for u in range(5)
+                                for v in range(5)
+                                if u != v])
+
+    def test_dense(self):
+        bqm = AdjArrayBQM([[.1, 1, 2], [0, 0, 0], [1, 1, 0]])
+
+        self.assertEqual(bqm.shape, (3, 3))
+
+        lin, quad = bqm.to_lists()
+        self.assertEqual(lin, [(0, .1), (2, 0), (4, 0)])
+        self.assertEqual(quad, [(1, 1), (2, 3),
+                                (0, 1), (2, 1),
+                                (0, 3), (1, 1)])
+
+
+# class TestEnergies(unittest.TestCase):
+#     def test_2path(self):
+#         bqm = AdjArrayBQM([[.1, -1], [0, -.2]])
+#         samples = [[-1, -1],
+#                    [-1, +1],
+#                    [+1, -1],
+#                    [+1, +1]]
+
+#         energies = bqm.energies(np.asarray(samples))
+
+#         np.testing.assert_array_almost_equal(energies, [-.9, .7, 1.3, -1.1])
+
+#     def test_5chain(self):
+#         arr = np.tril(np.triu(np.ones((5, 5)), 1), 1)
+#         bqm = AdjArrayBQM(arr)
+#         samples = [[-1, +1, -1, +1, -1]]
+
+#         energies = bqm.energies(np.asarray(samples))
+#         np.testing.assert_array_almost_equal(energies, [-4])
+
+#     def test_random(self):
+#         bqm = AdjArrayBQM([[0, -1, 0, 0],
+#                            [0, 0, .5, .2],
+#                            [0, 0, 0, 1.3],
+#                            [0, 0, 0, 0]])
+
+#         J = {(0, 1): -1, (1, 2): .5, (1, 3): .2, (2, 3): 1.3}
+
+#         for sample in itertools.product((-1, 1), repeat=len(bqm)):
+#             energy, = bqm.energies(np.atleast_2d(sample))
+
+#             target = sum(b*sample[u]*sample[v] for (u, v), b in J.items())
+
+#             self.assertAlmostEqual(energy, target)
