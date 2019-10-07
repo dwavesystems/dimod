8f24e549b2455e150c5c5efd117b3df60ccce6da
Hossein Sadeghi <hsadeghi@dwavesys.com>
2019-10-06 19:24:57 -0700

    Refactor ExactSolver

--- dimod/reference/samplers/exact_solver.py
+++ dimod/reference/samplers/exact_solver.py
@@ -23,8 +23,6 @@ Note:
 """
 import itertools
 
-import numpy as np
-from six.moves import zip
 
 from dimod.core.sampler import Sampler
 from dimod.decorators import bqm_index_labels
@@ -86,46 +84,18 @@ class ExactSolver(Sampler):
             :obj:`~dimod.SampleSet`
 
         """
-        M = bqm.binary.to_numpy_matrix()
-        off = bqm.binary.offset
-
-        if M.shape == (0, 0):
+        n = len(bqm)
+        if n == 0:
             return SampleSet.from_samples([], bqm.vartype, energy=[])
-
-        sample = np.zeros((len(bqm),), dtype=bool)
-
-        # now we iterate, flipping one bit at a time until we have
-        # traversed all samples. This is a Gray code.
-        # https://en.wikipedia.org/wiki/Gray_code
-        def iter_samples():
-            sample = np.zeros((len(bqm)), dtype=bool)
-            energy = 0.0
-
-            yield sample.copy(), energy + off
-
-            for i in range(1, 1 << len(bqm)):
-                v = _ffs(i)
-
-                # flip the bit in the sample
-                sample[v] = not sample[v]
-
-                # for now just calculate the energy, but there is a more clever way by calculating
-                # the energy delta for the single bit flip, don't have time, pull requests
-                # appreciated!
-                energy = sample.dot(M).dot(sample.transpose())
-
-                yield sample.copy(), float(energy) + off
-
-        samples, energies = zip(*iter_samples())
-
-        response = SampleSet.from_samples(np.array(samples, dtype='int8'), Vartype.BINARY, energies)
-
-        # make sure the response matches the given vartype, in-place.
+        bqm_copy = bqm.copy()
+        bqm_copy = bqm_copy.change_vartype(Vartype.BINARY)
+        samples = list(itertools.product([0, 1], repeat=n))
+        response = SampleSet.from_samples_bqm(samples, bqm_copy)
         response.change_vartype(bqm.vartype, inplace=True)
-
         return response
 
 
 def _ffs(x):
     """Gets the index of the least significant set bit of x."""
     return (x & -x).bit_length() - 1
+
