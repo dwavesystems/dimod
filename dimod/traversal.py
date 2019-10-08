e5dd5f39c374209b1a7c843c5f42a3687fa563d2
Alexander Condello <arcondello@gmail.com>
2019-10-08 10:40:11 -0700

    Add connected_components and bfs_variables functions

---
+++ dimod/traversal.py
@@ -0,0 +1,102 @@
+# The following traversal code is adapted from NetworkX's implementation. The
+# only thing that was preventing their implementation from using BQMs directly
+# was the not_implemented_for decorator. So rather than force the construction
+# of a new NetworkX graph, we just skip that decorator (and make some other
+# small changes). Modifications are licensed under the Apache 2.0 Software
+# license at the top.
+#
+# Copyright 2019 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# NetworkX license:
+#
+# Copyright (C) 2004-2019, NetworkX Developers
+# Aric Hagberg <hagberg@lanl.gov>
+# Dan Schult <dschult@colgate.edu>
+# Pieter Swart <swart@lanl.gov>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#
+#   * Neither the name of the NetworkX Developers nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+def connected_components(bqm):
+    """Yields sets of connected variables.
+
+    Args:
+        bqm (:obj:`dimod.BinaryQuadraticModel`):
+            A binary quadratic model.
+
+    Yields:
+        set: A set of variables in the bqm that form a connected component.
+
+    """
+    seen = set()
+    for v in bqm:
+        if v not in seen:
+            c = set(bfs_variables(bqm, v))
+            yield c
+            seen.update(c)
+
+
+def bfs_variables(bqm, source):
+    """Yields variables in breadth-first search order.
+
+    Args:
+        bqm (:obj:`dimod.BinaryQuadraticModel`):
+            A binary quadratic model.
+
+        source (variable):
+            A variable in the binary quadratic model.
+
+    Yields:
+        variable: variables in the bqm, yielded in breadth-first search order
+        starting at source.
+
+    """
+    seen = set()
+    nextlevel = {source}
+    while nextlevel:
+        thislevel = nextlevel
+        nextlevel = set()
+        for v in thislevel:
+            if v not in seen:
+                yield v
+                seen.add(v)
+                nextlevel.update(bqm.adj[v])
---
+++ tests/test_traversal.py
@@ -0,0 +1,43 @@
+# Copyright 2019 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# =============================================================================
+import unittest
+
+import dimod
+
+
+class TestBFS(unittest.TestCase):
+    def test_imbalanced_tree(self):
+        bqm = dimod.BQM.from_ising({}, {'ab': -1, 'ac': -1, 'bd': -1})
+
+        bfs = list(dimod.bfs_variables(bqm, 'a'))
+
+        self.assertIn(bfs, [['a', 'b', 'c', 'd'],
+                            ['a', 'c', 'b', 'd']])
+
+
+class TestConnectedComponents(unittest.TestCase):
+    def test_connected(self):
+        bqm = dimod.BQM.from_ising({}, {'ab': -1, 'ac': -1, 'bd': -1})
+
+        cc = list(dimod.connected_components(bqm))
+        self.assertEqual(cc, [set(bqm)])
+
+    def test_disconnected(self):
+        bqm = dimod.BQM.from_ising({}, {'ab': -1, 'ac': -1, 'de': -1})
+
+        cc = list(dimod.connected_components(bqm))
+        self.assertIn(cc, [[set(['a', 'b', 'c']), set(['d', 'e'])],
+                           [set(['d', 'e']), set(['a', 'b', 'c'])]])
