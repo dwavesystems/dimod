9e10c57c0791ecfe980cab0ab440a909927ca238
Alexander Condello <arcondello@gmail.com>
2020-04-24 13:19:39 -0700

    Update regex to exclude "." case
    Co-Authored-By: Radomir Stevanovic <radomir@dwavesys.com>

--- dimod/serialization/coo.py
+++ dimod/serialization/coo.py
@@ -78,7 +78,7 @@ from numbers import Integral
 from dimod import Vartype
 from dimod.binary_quadratic_model import BinaryQuadraticModel
 
-_LINE_REGEX = r'^\s*(\d+)\s+(\d+)\s+([+-]?(?:[0-9]*[.]?)[0-9]*)\s*$'
+_LINE_REGEX = r'^\s*(\d+)\s+(\d+)\s+([+-]?\d*(\.\d+)?)\s*$'
 """
 Each line should look like
 
