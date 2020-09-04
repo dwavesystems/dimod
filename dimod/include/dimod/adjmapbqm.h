2c447b8468d342e47ba2c92fcf3835d2cec5b96c
Alexander Condello <arcondello@gmail.com>
2020-09-04 08:53:55 -0700

    Add Adj*BQM::linear

--- dimod/include/dimod/adjmapbqm.h
+++ dimod/include/dimod/adjmapbqm.h
@@ -43,7 +43,7 @@ class AdjMapBQM {
         adj.resize(bqm.num_variables());
 
         for (variable_type v = 0; v < bqm.num_variables(); ++v) {
-            set_linear(v, bqm.get_linear(v));
+            linear(v) = bqm.linear(v);
 
             auto span = bqm.neighborhood(v);
             adj[v].first.insert(span.first, span.second);
@@ -94,8 +94,9 @@ class AdjMapBQM {
         return adj[v].first.size();
     }
 
+    [[deprecated("Use AdjMapBQM::linear(v)")]]
     bias_type get_linear(variable_type v) const {
-        return adj[v].second;
+        return linear(v);
     }
 
     std::pair<bias_type, bool>
@@ -110,6 +111,16 @@ class AdjMapBQM {
             return std::make_pair(0, false);
 
         return std::make_pair(it->second, true);
+    }
+
+    bias_type& linear(variable_type v) {
+        assert(v >= 0 && v < adj.size());
+        return adj[v].second;
+    }
+
+    const bias_type& linear(variable_type v) const {
+        assert(v >= 0 && v < adj.size());
+        return adj[v].second;
     }
 
     std::pair<outvars_iterator, outvars_iterator>
@@ -161,9 +172,10 @@ class AdjMapBQM {
         return false;
     }
 
+    [[deprecated("Use AdjMapBQM::linear(v)")]]
     void set_linear(variable_type v, bias_type b) {
-        assert(v >= 0 && v < invars.size());
-        adj[v].second = b;
+        assert(v >= 0 && v < adj.size());
+        linear(v) = b;
     }
 
     bool set_quadratic(variable_type u, variable_type v, bias_type b) {
