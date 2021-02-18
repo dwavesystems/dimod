bce9e922caffcd41e986430a00a81a53b2ff4385
hsadeghidw <hsadeghi@dwavesys.com>
2021-02-18 14:37:29 -0800

    Add DQM.add_linear_equality_constraint test cases

--- tests/test_discrete_quadratic_model.py
+++ tests/test_discrete_quadratic_model.py
@@ -563,6 +563,74 @@ class TestConstraint(unittest.TestCase):
         # discard that interaction
         self.assertEqual(dqm.num_case_interactions(), 2)
 
+    def case_1():
+        num_variables = 4
+        cases = np.random.randint(3, 6, size=num_variables)
+        dqm_0 = dimod.DiscreteQuadraticModel()
+        dqm = dimod.DiscreteQuadraticModel()
+        for i in range(num_variables):
+            dqm_0.add_variable(cases[i], i)
+            dqm.add_variable(cases[i], i)
+        x = dqm.variables
+
+        expression1 = [(0, 0, 2), (1, 0, 2), (2, 1, 0), (3, 0, 8)]
+        expression2 = [(0, 1, 2), (1, 1, 2), (2, 0, 0), (3, 1, 8)]
+        expression = expression1 + expression2
+        constant = np.random.randint(1, 10) * num_variables
+        lagrange_multiplier = np.random.randint(1, 10)
+        dqm.add_linear_equality_constraint(
+            expression,
+            lagrange_multiplier=lagrange_multiplier, constant=constant)
+
+        expression_dict1 = {v: (c, b) for v, c, b in expression1}
+        expression_dict2 = {v: (c, b) for v, c, b in expression2}
+
+        for case_values in itertools.product(*(range(c) for c in cases)):
+            state = {x[i]: case_values[i] for i in x}
+            energy = dqm.energy(state) + lagrange_multiplier * constant ** 2
+            s = constant
+            for v, cv, bias in expression1:
+                if expression_dict1[v][0] == state[v]:
+                    s += bias
+            for v, cv, bias in expression2:
+                if expression_dict2[v][0] == state[v]:
+                    s += bias
+            assert (energy == lagrange_multiplier * s ** 2 + dqm_0.energy(state))
+
+    def case_2():
+        num_variables = 4
+        cases = np.random.randint(3, 6, size=num_variables)
+        dqm_0 = dimod.DiscreteQuadraticModel()
+        dqm = dimod.DiscreteQuadraticModel()
+        for i in range(num_variables):
+            dqm_0.add_variable(cases[i], i)
+            dqm.add_variable(cases[i], i)
+        x = dqm.variables
+
+        expression1 = [(0, 0, 2), (1, 0, 2), (2, 1, 0), (3, 0, 8)]
+        expression2 = [(0, 0, 2), (1, 0, 2), (2, 1, 0), (3, 0, 8)]
+        expression = expression1 + expression2
+        constant = np.random.randint(1, 10) * num_variables
+        lagrange_multiplier = np.random.randint(1, 10)
+        dqm.add_linear_equality_constraint(
+            expression,
+            lagrange_multiplier=lagrange_multiplier, constant=constant)
+
+        expression_dict1 = {v: (c, b) for v, c, b in expression1}
+        expression_dict2 = {v: (c, b) for v, c, b in expression2}
+
+        for case_values in itertools.product(*(range(c) for c in cases)):
+            state = {x[i]: case_values[i] for i in x}
+            energy = dqm.energy(state) + lagrange_multiplier * constant ** 2
+            s = constant
+            for v, cv, bias in expression1:
+                if expression_dict1[v][0] == state[v]:
+                    s += bias
+            for v, cv, bias in expression2:
+                if expression_dict2[v][0] == state[v]:
+                    s += bias
+            assert (energy == lagrange_multiplier * s ** 2 + dqm_0.energy(state))
+
 
 class TestNumpyVectors(unittest.TestCase):
 
