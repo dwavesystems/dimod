5b5f3f89773ef630225d8de8fa6160c35fcaec27
Alexander Condello <arcondello@gmail.com>
2021-05-04 16:00:47 -0700

    Integrate sort_vector_pairs into zip_sort

--- dimod/include/dimod/utils.h
+++ dimod/include/dimod/utils.h
@@ -29,32 +29,14 @@ namespace utils {
         return ub.first < v;
     }
 
-    template <class T1, class T2>
-    void zip_sort(std::vector<T1> &keys, std::vector<T2> &values) {
-        std::size_t size = std::min(keys.size(), values.size());
-
-        std::vector<std::pair<T1, T2>> zipped;
-        zipped.reserve(size);
-
-        for (std::size_t i = 0; i < size; ++i) {
-            zipped.emplace_back(keys[i], values[i]);
-        }
-
-        std::sort(zipped.begin(), zipped.end());
-
-        for (std::size_t i = 0; i < size; ++i) {
-            keys[i] = zipped[i].first;
-            values[i] = zipped[i].second;
-        }
-    }
-
-    // This code is a modification of the code found here :
+    // zip_sort is a modification of the code found here :
     // https://www.geeksforgeeks.org/iterative-quick-sort/
-    template <class C_t, class R_t>
-    void sort_vector_pairs(std::vector<C_t>& control,
-                           std::vector<R_t>& response) {
+
+    /// Sort two vectors, using `control` to provide the ordering.
+    template <class T1, class T2>
+    void zip_sort(std::vector<T1>& control, std::vector<T2>& response) {
         assert(control.size() == response.size());
-        size_t length = control.size();
+        std::size_t length = control.size();
 
         if (length < 2) {
             return;
@@ -64,11 +46,11 @@ namespace utils {
                     "overflow will occur.");
         }
 
-        long long int* stack =
-                (long long int*)malloc(length * sizeof(long long int));
-        long long int top = -1;
-        long long int low = 0;
-        long long int high = length - 1;
+        std::int64_t* stack =
+                (std::int64_t*)malloc(length * sizeof(std::int64_t));
+        std::int64_t top = -1;
+        std::int64_t low = 0;
+        std::int64_t high = length - 1;
 
         stack[++top] = low;
         stack[++top] = high;
@@ -77,25 +59,25 @@ namespace utils {
             high = stack[top--];
             low = stack[top--];
 
-            long long int pivot_choice_index = (low + high + 1) / 2;
+            std::int64_t pivot_choice_index = (low + high + 1) / 2;
             if (pivot_choice_index != high) {
-                C_t control_temp = control[pivot_choice_index];
+                T1 control_temp = control[pivot_choice_index];
                 control[pivot_choice_index] = control[high];
                 control[high] = control_temp;
             }
 
-            C_t pivot_element = control[high];
-            long long int i = low - 1;
+            T1 pivot_element = control[high];
+            std::int64_t i = low - 1;
 
-            for (long long int j = low; j <= high - 1; j++) {
+            for (std::int64_t j = low; j <= high - 1; j++) {
                 if (control[j] <= pivot_element) {
                     i++;
                     if (i != j) {
-                        C_t control_temp = control[i];
+                        T1 control_temp = control[i];
                         control[i] = control[j];
                         control[j] = control_temp;
 
-                        R_t response_temp = response[i];
+                        T2 response_temp = response[i];
                         response[i] = response[j];
                         response[j] = response_temp;
                     }
@@ -103,13 +85,13 @@ namespace utils {
             }
 
             // Pivot position.
-            long long int p = i + 1;
+            std::int64_t p = i + 1;
 
-            C_t control_temp = control[p];
+            T1 control_temp = control[p];
             control[p] = control[high];
             control[high] = control_temp;
 
-            R_t response_temp = response[p];
+            T2 response_temp = response[p];
             response[p] = response[high];
             response[high] = response_temp;
 
