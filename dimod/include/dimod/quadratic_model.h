6300df9a019aa314362bc17aae25244dc309c957
Alexander Condello <arcondello@gmail.com>
2021-05-04 10:29:33 -0700

    Integrate review comments into C++ BQM and add docstrings

--- dimod/include/dimod/quadratic_model.h
+++ dimod/include/dimod/quadratic_model.h
@@ -22,18 +22,37 @@
 
 namespace dimod {
 
-enum Vartype { BINARY, SPIN, INTEGER };
+/// Encode the domain of a variable.
+enum Vartype {
+    BINARY,  ///< Variables that are either 0 or 1.
+    SPIN,    ///< Variables that are either -1 or 1.
+    INTEGER  ///< Variables that are integer valued.
+};
 
+/**
+ * Used internally by QuadraticModelBase to sparsely encode the neighborhood of 
+ * a variable.
+ *
+ * Internally, Neighborhoods keep two vectors, one of neighbors and the other
+ * of biases. Neighborhoods are designed to make access more like a standard
+ * library map.
+ */
 template <class Bias, class Neighbor>
 class Neighborhood {
  public:
+    /// The first template parameter (Bias).
     using bias_type = Bias;
+
+    /// The second template parameter (Neighbor).
     using neighbor_type = Neighbor;
+
+    /// Unsigned integral type that can represent non-negative values.
     using size_type = std::size_t;
 
-    struct iterator {
-        // this could be random access but let's keep it simple for now
+ private:
+    struct Iterator {
         using iterator_category = std::forward_iterator_tag;
+
         using difference_type = std::ptrdiff_t;
         using value_type = std::pair<const neighbor_type&, bias_type&>;
         using pointer = value_type*;
@@ -43,27 +62,27 @@ class Neighborhood {
         using neighbor_iterator =
                 typename std::vector<neighbor_type>::const_iterator;
 
-        iterator(neighbor_iterator nit, bias_iterator bit)
+        Iterator(neighbor_iterator nit, bias_iterator bit)
                 : neighbor_it_(nit), bias_it_(bit) {}
 
-        iterator& operator++() {
+        Iterator& operator++() {
             bias_it_++;
             neighbor_it_++;
             return *this;
         }
 
-        iterator operator++(int) {
-            iterator tmp = *this;
+        Iterator operator++(int) {
+            Iterator tmp = *this;
             ++(*this);
             return tmp;
         }
 
-        friend bool operator==(const iterator& a, const iterator& b) {
+        friend bool operator==(const Iterator& a, const Iterator& b) {
             return a.neighbor_it_ == b.neighbor_it_ && a.bias_it_ == b.bias_it_;
         }
 
-        friend bool operator!=(const iterator& a, const iterator& b) {
-            return a.neighbor_it_ != b.neighbor_it_ && a.bias_it_ != b.bias_it_;
+        friend bool operator!=(const Iterator& a, const Iterator& b) {
+            return !(a == b);
         }
 
         value_type operator*() { return value_type{*neighbor_it_, *bias_it_}; }
@@ -75,8 +94,7 @@ class Neighborhood {
         friend class Neighborhood;
     };
 
-    struct const_iterator {
-        // this could be random access but let's keep it simple for now
+    struct ConstIterator {
         using iterator_category = std::forward_iterator_tag;
         using difference_type = std::ptrdiff_t;
         using value_type = std::pair<const neighbor_type&, const bias_type&>;
@@ -87,31 +105,31 @@ class Neighborhood {
         using neighbor_iterator =
                 typename std::vector<neighbor_type>::const_iterator;
 
-        const_iterator() {}
+        ConstIterator() {}
 
-        const_iterator(neighbor_iterator nit, bias_iterator bit)
+        ConstIterator(neighbor_iterator nit, bias_iterator bit)
                 : neighbor_it_(nit), bias_it_(bit) {}
 
-        const_iterator& operator++() {
+        ConstIterator& operator++() {
             bias_it_++;
             neighbor_it_++;
             return *this;
         }
 
-        const_iterator operator++(int) {
-            const_iterator tmp = *this;
+        ConstIterator operator++(int) {
+            ConstIterator tmp = *this;
             ++(*this);
             return tmp;
         }
 
-        friend bool operator==(const const_iterator& a,
-                               const const_iterator& b) {
+        friend bool operator==(const ConstIterator& a,
+                               const ConstIterator& b) {
             return a.neighbor_it_ == b.neighbor_it_ && a.bias_it_ == b.bias_it_;
         }
 
-        friend bool operator!=(const const_iterator& a,
-                               const const_iterator& b) {
-            return a.neighbor_it_ != b.neighbor_it_ && a.bias_it_ != b.bias_it_;
+        friend bool operator!=(const ConstIterator& a,
+                               const ConstIterator& b) {
+            return !(a == b);
         }
 
         const value_type operator*() const {
@@ -125,6 +143,20 @@ class Neighborhood {
         friend class Neighborhood;
     };
 
+ public:
+    /// A forward iterator to `pair<const neighbor_type&, bias_type&>`.
+    using iterator = Iterator;
+
+    /// A forward iterator to `const pair<const neighbor_type&, const
+    /// bias_type&>.`
+    using const_iterator = ConstIterator;
+
+    /**
+     * Return a reference to the bias associated with `v`.
+     *
+     * This function automatically checks whether `v` is a variable in the
+     * neighborhood and throws a `std::out_of_range` exception if it is not.
+     */
     bias_type at(neighbor_type v) const {
         auto it = std::lower_bound(neighbors.begin(), neighbors.end(), v);
         size_type idx = std::distance(neighbors.begin(), it);
@@ -137,31 +169,47 @@ class Neighborhood {
         }
     }
 
+    /// Returns an iterator to the beginning.
     iterator begin() {
         return iterator{neighbors.cbegin(), quadratic_biases.begin()};
     }
 
+    /// Returns an iterator to the end.
     iterator end() {
         return iterator{neighbors.cend(), quadratic_biases.end()};
     }
 
+    /// Returns a const_iterator to the beginning.
     const_iterator cbegin() const {
         return const_iterator{neighbors.cbegin(), quadratic_biases.cbegin()};
     }
 
+    /// Returns a const_iterator to the end.
     const_iterator cend() const {
         return const_iterator{neighbors.cend(), quadratic_biases.cend()};
     }
 
+    /**
+     * Insert a neighbor, bias pair at the end of the neighborhood.
+     *
+     * Note that this does not keep the neighborhood self-consistent and should
+     * only be used when you know that the neighbor is greater than the current
+     * last element.
+     */
     void emplace_back(neighbor_type v, bias_type bias) {
         neighbors.push_back(v);
         quadratic_biases.push_back(bias);
     }
 
+    /**
+     * Erase an element from the neighborhood.
+     *
+     * Returns the number of element removed, either 0 or 1.
+     */
     size_type erase(neighbor_type v) {
         auto it = std::lower_bound(neighbors.begin(), neighbors.end(), v);
         if (it != neighbors.end() && (*it) == v) {
-            // is is there to erase
+            // is there to erase
             size_type idx = std::distance(neighbors.begin(), it);
 
             neighbors.erase(it);
@@ -173,19 +221,25 @@ class Neighborhood {
         }
     }
 
+    /// Erase elements from the neighborhood.
     void erase(iterator first, iterator last) {
         quadratic_biases.erase(first.bias_it_, last.bias_it_);
         neighbors.erase(first.neighbor_it_, last.neighbor_it_);
     }
 
-    /// return an iterator to the first element that does not come before v
+    /// Return an iterator to the first element that does not come before `v`.
     iterator lower_bound(neighbor_type v) {
         auto it = std::lower_bound(neighbors.begin(), neighbors.end(), v);
         return iterator{it, quadratic_biases.begin() +
                                     std::distance(neighbors.begin(), it)};
     }
 
-    // returns default if missing without inserting
+    /**
+     * Return the bias at neighbor `v` or the default value.
+     *
+     * Return the bias of `v` if `v` is in the neighborhood, otherwise return
+     * the `value` provided without inserting `v`.
+     */
     bias_type get(neighbor_type v, bias_type value = 0) const {
         auto it = std::lower_bound(neighbors.begin(), neighbors.end(), v);
         size_type idx = std::distance(neighbors.begin(), it);
@@ -198,9 +252,16 @@ class Neighborhood {
         }
     }
 
+    /// Return the size of the neighborhood.
     size_type size() const { return neighbors.size(); }
 
-    // inserts if missing
+    /**
+     * Access the bias of `v`.
+     *
+     * If `v` is in the neighborhood, the function returns a reference to its
+     * bias. If `v` is not in the neighborhood, it is inserted and a reference
+     * is returned to its bias.
+     */
     bias_type& operator[](neighbor_type v) {
         auto it = std::lower_bound(neighbors.begin(), neighbors.end(), v);
         size_type idx = std::distance(neighbors.begin(), it);
@@ -221,10 +282,16 @@ class Neighborhood {
 template <class Bias, class Neighbor = std::size_t>
 class QuadraticModelBase {
  public:
+    /// The first template parameter (Bias)
     using bias_type = Bias;
+
+    /// The second template parameter (Neighbor)
     using neighbor_type = Neighbor;
+
+    /// Unsigned integral type that can represent non-negative values.
     using size_type = std::size_t;
 
+    /// A forward iterator to `pair<const neighbor_type&, bias_type&>`
     using const_neighborhood_iterator =
             typename Neighborhood<bias_type, neighbor_type>::const_iterator;
 
@@ -243,12 +310,11 @@ class QuadraticModelBase {
     /**
      * Return the energy of the given sample.
      *
-     * @param sample_start A random access iterator pointing to the beginning
-     *     of the sample.
-     * @return The energy of the given sample.
+     * The `sample_start` must be random access iterator pointing to the
+     * beginning of the sample.
      *
      * The behavior of this function is undefined when the sample is not
-     * <num_variables>"()" long.
+     * `num_variables()` long.
      */
     template <class Iter>  // todo: allow different return types
     bias_type energy(Iter sample_start) {
@@ -274,11 +340,10 @@ class QuadraticModelBase {
     /**
      * Return the energy of the given sample.
      *
-     * @param sample A vector containing the sample.
-     * @return The energy of the given sample.
+     * The `sample` must be a vector containing the sample.
      *
      * The behavior of this function is undefined when the sample is not
-     * <num_variables>"()" long.
+     * `num_variables()` long.
      */
     template <class T>
     bias_type energy(const std::vector<T>& sample) {
@@ -301,12 +366,11 @@ class QuadraticModelBase {
     /**
      * Return the quadratic bias associated with `u`, `v`.
      *
+     * If `u` and `v` do not have a quadratic bias, returns 0.
+     *
      * Note that this function does not return a reference, this is because
      * each quadratic bias is stored twice.
      *
-     * @param u A variable in the binary quadratic model.
-     * @param v A variable in the binary quadratic model.
-     * @return The quadratic bias if it exists, otherwise 0.
      */
     bias_type quadratic(size_type u, neighbor_type v) const {
         return adj_[u].get(v);
@@ -318,12 +382,8 @@ class QuadraticModelBase {
      * Note that this function does not return a reference, this is because
      * each quadratic bias is stored twice.
      *
-     * @param u A variable in the binary quadratic model.
-     * @param v A variable in the binary quadratic model.
-     * @return The quadratic bias if it exists, otherwise 0.
-     * @exception out_of_range If either `u` or `v` are not variables or if
-     *     they do not have an interaction then the function throws an
-     *     exception.
+     * Raises an `out_of_range` error if either `u` or `v` are not variables or
+     * if they do not have an interaction then the function throws an exception.
      */
     bias_type quadratic_at(size_type u, size_type v) const {
         return adj_[u].at(v);
@@ -368,29 +428,51 @@ class QuadraticModelBase {
     bias_type offset_;
 };
 
-/*
- * A binary quadratic model.
+/**
+ * A Binary Quadratic Model is a quadratic polynomial over binary variables.
+ *
+ * Internally, BQMs are stored in a vector-of-vectors adjacency format.
+ *
  */
 template <class Bias, class Neighbor = std::size_t>
 class BinaryQuadraticModel : public QuadraticModelBase<Bias, Neighbor> {
- private:
+ public:
+    /// The type of the base class.
     using base_type = QuadraticModelBase<Bias, Neighbor>;
 
- public:
+    /// The first template parameter (Bias).
     using bias_type = typename base_type::bias_type;
+
+    /// Unsigned integral that can represent non-negative values.
     using size_type = typename base_type::size_type;
     using neighbor_type = typename base_type::neighbor_type;
 
+    /// Empty constructor. The vartype defaults to `Vartype::BINARY`.
     BinaryQuadraticModel() : base_type(), vartype_(Vartype::BINARY) {}
 
+    /// Create a BQM of the given `vartype`.
     explicit BinaryQuadraticModel(Vartype vartype)
             : base_type(), vartype_(vartype) {}
 
+    /// Create a BQM with `n` variables of the given `vartype`.
     BinaryQuadraticModel(size_type n, Vartype vartype)
             : BinaryQuadraticModel(vartype) {
         resize(n);
     }
 
+    /**
+     * Create a BQM from a dense matrix.
+     *
+     * `dense` must be an array of length `num_variables^2`.
+     *
+     * Values on the diagonal are treated differently depending on the variable
+     * type.
+     * If the BQM is SPIN-valued, then the values on the diagonal are
+     * added to the offset.
+     * If the BQM is BINARY-valued, then the values on the diagonal are added
+     * as linear biases.
+     *
+     */
     template <class T>
     BinaryQuadraticModel(const T dense[], size_type num_variables,
                          Vartype vartype)
@@ -398,6 +480,7 @@ class BinaryQuadraticModel : public QuadraticModelBase<Bias, Neighbor> {
         add_quadratic(dense, num_variables);
     }
 
+    /// Add quadratic bias for the given variables.
     void add_quadratic(size_type u, size_type v, bias_type bias) {
         if (u == v) {
             if (vartype_ == Vartype::BINARY) {
@@ -416,7 +499,17 @@ class BinaryQuadraticModel : public QuadraticModelBase<Bias, Neighbor> {
     /*
      * Add quadratic biases to the BQM from a dense matrix.
      *
-     * Variables must already be present.
+     * `dense` must be an array of length `num_variables^2`.
+     *
+     * The behavior of this method is undefined when the bqm has fewer than
+     * `num_variables` variables.
+     *
+     * Values on the diagonal are treated differently depending on the variable
+     * type.
+     * If the BQM is SPIN-valued, then the values on the diagonal are
+     * added to the offset.
+     * If the BQM is BINARY-valued, then the values on the diagonal are added
+     * as linear biases.
      */
     template <class T>
     void add_quadratic(const T dense[], size_type num_variables) {
@@ -460,6 +553,7 @@ class BinaryQuadraticModel : public QuadraticModelBase<Bias, Neighbor> {
         }
     }
 
+    /// Change the vartype of the binary quadratic model
     void change_vartype(Vartype vartype) {
         if (vartype == vartype_) return;  // nothing to do
 
@@ -518,9 +612,14 @@ class BinaryQuadraticModel : public QuadraticModelBase<Bias, Neighbor> {
         base_type::adj_.resize(n);
     }
 
+    /// Set the quadratic bias for the given variables.
     void set_quadratic(size_type u, size_type v, bias_type bias) {
         if (u == v) {
-            throw std::logic_error("not implemented yet");
+            // unlike add_quadratic, this is not really well defined for
+            // binary quadratic models. I.e. if there is a linear bias, do we
+            // overwrite? Or?
+            throw std::domain_error(
+                    "Cannot set the quadratic bias of a variable with itself");
         } else {
             base_type::adj_[u][v] = bias;
             base_type::adj_[v][u] = bias;
