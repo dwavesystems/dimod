72d20fbbce96f321795eaa165b6ad0b3847c05fb
jpasvolsky <jpasvolsky@dwavesys.com>
2021-05-11 12:19:06 -0700

    Update cookie methods

--- docs/conf.py
+++ docs/conf.py
@@ -136,8 +136,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 def setup(app):
-   app.add_stylesheet('cookie_notice.css')
-   app.add_javascript('cookie_notice.js')
+   app.add_css_file('cookie_notice.css')
+   app.add_js_file('cookie_notice.js')
    app.add_config_value('target', 'repo', 'env')
 
 # -- Options for HTMLHelp output ------------------------------------------
