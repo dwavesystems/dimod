6d1e4b1fccbfa556599533f934f15143db1a0ef6
Heidi Tong <htong@dwavesys.com>
2021-05-21 09:42:59 -0700

    Update version 0.9.14 -> 0.9.15
    Fixes
    -----
    * Windows wheels now correctly include `.pxd` files. #826

--- dimod/package_info.py
+++ dimod/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.9.14'
+__version__ = '0.9.15'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'A shared API for binary quadratic model samplers.'
