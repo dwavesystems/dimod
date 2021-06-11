43c5c57e8b2bd5c29e25b54275fbbbc534f42fdc
Alexander Condello <arcondello@gmail.com>
2021-06-11 14:58:32 -0700

    Update version 0.10.0.dev4 -> 0.10.0.dev5
    New Features
    ------------
    
    * Add `add_bqm` method to C++ BinaryQuadraticModel https://github.com/dwavesystems/dimod/pull/821, https://github.com/dwavesystems/dimod/pull/823
    * Add `Structured.valid_bqm_graph` method for verifying input problem structure https://github.com/dwavesystems/dimod/pull/832
    * Reimplement `BinaryQuadraticModel` to use new C++ code https://github.com/dwavesystems/dimod/pull/828
    * `BinaryQuadraticModel` can now be manipulated symbolically https://github.com/dwavesystems/dimod/pull/834
    * `load` function can now load all model types https://github.com/dwavesystems/dimod/pull/841, https://github.com/dwavesystems/dimod/pull/843
    * `DiscreteQuadraticModel` now has an `.offset` attribute https://github.com/dwavesystems/dimod/pull/838
    * Add `ConstrainedQuadraticModel` class https://github.com/dwavesystems/dimod/pull/839
    
    Fix
    ---
    * Fix type promotions in binary quadratic models with object biases https://github.com/dwavesystems/dimod/pull/836

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 
 # version is used by serialization below so we need it before everything
-__version__ = '0.10.0.dev4'
+__version__ = '0.10.0.dev5'
 
 from dimod.bqm import *
 
