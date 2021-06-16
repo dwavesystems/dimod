0321812b0c21c96ca7928dab9e4e75054d6d4881
Alexander Condello <arcondello@gmail.com>
2021-06-15 19:24:00 -0700

    Update __init__.py
    Changes
    -------
    * Promote `dimod.binary.binary_quadratic_model` to the top-level `dimod` namespace and remove `dimod.binary_quadratic_model.BinaryQuadraticModel` https://github.com/dwavesystems/dimod/pull/847
    
    Fixes
    -----
    * Fix `Variables._relabel` in rare cases https://github.com/dwavesystems/dimod/commit/a0791b0045892d383ad37a53da1bf0ff6c0a2bdb

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 
 # version is used by serialization below so we need it before everything
-__version__ = '0.10.0.dev5'
+__version__ = '0.10.0.dev6'
 
 from dimod.bqm import *
 
