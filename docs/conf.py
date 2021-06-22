33ac475d8505345afc66747391dfce7bdd4676b8
Alexander Condello <arcondello@gmail.com>
2021-06-22 10:03:21 -0700

    Fix linkcheck

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sdk_index.rst']
 linkcheck_retries = 2
 linkcheck_anchors = False
 linkcheck_ignore = [r'https://cloud.dwavesys.com/leap',  # redirects, many checks
-                    r'https://docs.ocean.dwavesys.com/projects/dimod'
+                    r'https://docs.ocean.dwavesys.com/projects/dimod',
+                    r'.clang-format',
+                    r'setup.cfg',
                     ]  # dimod RTD currently not building on boost
 
 # The name of the Pygments (syntax highlighting) style to use.
