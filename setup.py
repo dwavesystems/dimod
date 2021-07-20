0961f4ef7cda113fbae43f5350773519485c4793
Heidi Tong <htong@dwavesys.com>
2021-07-20 14:26:27 -0700

    Remove support for numpy 1.21.0 and 1.21.1 due to typing bug

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'dimod/include/',
         ],
     install_requires=[
-        'numpy>=1.17.3,<2.0.0',  # keep synced with circle-ci, pyproject.toml
+        'numpy>=1.17.3,<2.0.0,!=1.21.0,!=1.21.1' # keep synced with circle-ci, pyproject.toml
         ],
     extras_require=dict(preprocessing='dwave-preprocessing>=0.3,<0.4'),
 )
