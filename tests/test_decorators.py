911cd010f1fd28f55be8382e6bf42cfa304082eb
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2021-11-30 14:02:02 -0800

    Add unittests from@arcondello

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -48,6 +48,20 @@ class IndexSampler(dimod.NullSampler):
 class TestBQMIndexLabels(unittest.TestCase):
     pass
 
+class EmptyStructuredSampler(dimod.Structured):
+    edgelist = nodelist = []
+
+    @dimod.decorators.bqm_structured
+    def sample(self, bqm):
+        pass
+
+class StructuredSampler(dimod.Structured):
+    nodelist = ['a', 'b', 'c']
+    edgelist = [('a', 'b')]
+
+    @dimod.decorators.bqm_structured
+    def sample(self, bqm):
+        pass
 
 class TestBQMIndexLabelledInput(unittest.TestCase):
     # we really should add proper tests but since it's deprecated anyway let's
@@ -364,3 +378,29 @@ class TestForwardingMethod(unittest.TestCase):
     def test_output(self):
         outer = self.outer
         self.assertEqual(outer.func(2, 3), 5)
+
+class TestBQMStructured(unittest.TestCase):
+    def test_empty_sampler_empty_bqm(self):
+        EmptyStructuredSampler().sample(dimod.BQM.empty('SPIN'))  # should do nothing
+
+    def test_empty_sampler(self):
+        with self.assertRaises(dimod.exceptions.BinaryQuadraticModelStructureError):
+            EmptyStructuredSampler().sample(dimod.BQM({'a': 1}, {}, 0.0, 'BINARY'))
+
+    def test_bqm_empty(self):
+        StructuredSampler().sample(dimod.BQM('BINARY'))  # should do nothing
+
+    def test_bqm_subset(self):
+        StructuredSampler().sample(dimod.BQM({'a': 1}, {'ab': 1}, 0, 'BINARY'))  # should do nothing
+
+    def test_bqm_extra_edge(self):
+        with self.assertRaises(dimod.exceptions.BinaryQuadraticModelStructureError):
+            StructuredSampler().sample(dimod.BQM({}, {'ca': 1}, 0.0, 'BINARY'))
+
+    def test_bqm_extra_node(self):
+        with self.assertRaises(dimod.exceptions.BinaryQuadraticModelStructureError):
+            StructuredSampler().sample(dimod.BQM({'a': 1, 'd': 1}, {}, 0.0, 'BINARY'))
+
+    def test_bqm_extra_node_disjoint(self):
+        with self.assertRaises(dimod.exceptions.BinaryQuadraticModelStructureError):
+            StructuredSampler().sample(dimod.BQM({'d': 1}, {}, 0.0, 'BINARY'))
