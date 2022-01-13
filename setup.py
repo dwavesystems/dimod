0442e7c4bfbcf362ab68a0c70671d2881af08b17
Alexander Condello <arcondello@gmail.com>
2022-01-13 10:36:54 -0800

    Raise minimum Python version to 3.7 and minimum NumPy version to 1.19.1
    Python 3.6 reached EOL Dec 2021
    
    The NumPy minimum was chosen to still support
    https://github.com/dwavesystems/penaltymodel/issues/134

--- setup.py
+++ setup.py
@@ -83,8 +83,7 @@ setup(
         'dimod/include/',
         ],
     install_requires=[
-        # ignoring 1.21.0 and 1.21.1 due to https://github.com/dwavesystems/dimod/issues/901
-        'numpy>=1.17.3,<2.0.0,!=1.21.0,!=1.21.1', # keep synced with circle-ci, pyproject.toml
+        'numpy>=1.19.1,<2.0.0',
         'pyparsing>=2.4.7, <3.0.0',
         ],
     # we use the generic 'all' so that in the future when we remove
