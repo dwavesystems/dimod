faa2cf7d45ba2984265ad1287150e43c837fa42d
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-02-10 11:16:53 -0800

    Update generators.knapsack, multi-knapsack

--- dimod/generators/knapsack.py
+++ dimod/generators/knapsack.py
@@ -29,27 +29,27 @@ def random_knapsack(num_items: int,
                     weight_range: Tuple[int, int] = (10, 30),
                     tightness_ratio: float = 0.5,
                     ) -> ConstrainedQuadraticModel:
-    """Returns a Constrained Quadratic Model encoding a knapsack problem.
+    """Generates a constrained quadratic model encoding a knapsack problem.
 
-    Given the number of items, the code generates a random knapsack problem,
-    formulated as a Constrained Quadratic model. The capacity of the bin is set
+    Given the number of items, generates a random knapsack problem, formulated as
+    a :class:`~dimod.ConstrainedQuadraticModel`. The capacity of bins is set
     to be ``tightness_ratio`` times the sum of the weights.
 
     Args:
         num_items: Number of items to choose from.
 
-        seed: Seed for numpy random number generator.
+        seed: Seed for NumPy random number generator.
 
-        value_range: The range of the randomly generated values for each item.
+        value_range: Range of the randomly generated values for each item.
 
-        weight_range: The range of the randomly generated weights for each item.
+        weight_range: Range of the randomly generated weights for each item.
 
-        tightness_ratio: ratio of capacity over sum of weights.
+        tightness_ratio: Ratio of capacity over sum of weights.
 
     Returns:
         The quadratic model encoding the knapsack problem. Variables are
-        denoted as ``x_{i}`` where ``x_{i} == 1`` means that the item ``i`` has
-        been placed in the knapsack.
+        denoted as ``x_{i}`` where ``x_{i} == 1`` means that item ``i`` is
+        placed in the knapsack.
 
     """
 
@@ -78,8 +78,8 @@ def random_knapsack(num_items: int,
 # Once the deprecation period is over we can use the knapsack with a different
 # api.
 def knapsack(*args, **kwargs) -> ConstrainedQuadraticModel:
-    warnings.warn("knapsack was deprecated after 0.10.6 and will be removed in 0.11.0, "
-                  "use random_bin_packing instead.",
+    warnings.warn("knapsack was deprecated after 0.10.6 and will be removed in 0.11.0. "
+                  "Use random_bin_packing instead.",
                   DeprecationWarning,
                   stacklevel=2)
     return random_knapsack(*args, **kwargs)
