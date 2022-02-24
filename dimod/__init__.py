e29c2f2a3c24257726d419669b1d65bdf980390c
Alexander Condello <arcondello@gmail.com>
2022-02-24 11:39:15 -0800

    Update version to 0.11.0.dev0

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 
 # version is used by serialization below so we need it before everything
-__version__ = '0.10.16'
+__version__ = '0.11.0.dev0'
 
 from dimod.bqm import *
 
