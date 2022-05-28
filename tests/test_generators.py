c805857822749df92b9c9407c26c77cca0700e3f
Jack Raymond <10591246+jackraymond@users.noreply.github.com>
2022-05-28 10:51:23 -0700

    test working finally

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -954,7 +954,7 @@ class TestSatisfiability(unittest.TestCase):
         num_var = 6 # test is 2^num_var, keep small.
         num_clauses = 24 #Deep in UNSAT phase (num_clause/num_var>>2.1), very unlikely to be SAT by chance.
         
-        bqm = dimod.generators.random_nae3sat(num_var, num_clauses, seed=seed, is_planted=True)
+        bqm = dimod.generators.random_nae3sat(num_var, num_clauses, is_planted=True)
         E_SAT = - num_clauses;
         all_energies = bqm.energies((np.array(list(itertools.product([-1,1], repeat=num_var))),bqm.variables))
         self.assertEqual(np.min(all_energies),E_SAT)
@@ -962,7 +962,7 @@ class TestSatisfiability(unittest.TestCase):
         self.assertEqual(all_energies[-1],E_SAT) #all 1 state
         
         num_clauses = 12 #Deep in UNSAT phase (num_clause/num_var>>0.9), very unlikely to be SAT by chance.
-        bqm = dimod.generators.random_2in4sat(num_var, num_clauses, seed=seed, is_planted=True)
+        bqm = dimod.generators.random_2in4sat(num_var, num_clauses, is_planted=True)
         E_SAT = - 2*num_clauses;
         all_energies = bqm.energies((np.array(list(itertools.product([-1,1], repeat=num_var))),bqm.variables))
         self.assertEqual(np.min(all_energies),E_SAT)
