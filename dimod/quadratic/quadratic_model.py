dwavesystems/dimod

commit 77fd6af995c2b940dedc4975d320c690aa019b31

Add default_vartype to QM.add_linear() and QM.add_linear_from()

dimod/quadratic/quadratic_model.py
@@ -398,20 +398,42 @@ class QuadraticModel(QuadraticViewsMixin):
         return self.data.variables
 
     @forwarding_method
-    def add_linear(self, v: Variable, bias: Bias):
+    def add_linear(self, v: Variable, bias: Bias, *,
+                   default_vartype=None,
+                   default_lower_bound=None,
+                   default_upper_bound=None,
+                   ):
         """Add a linear bias to an existing variable.
 
         Args:
             v: Variable label.
             bias: Linear bias for the variable.
+            default_vartype: The vartype of any variables not already in the
+                model. If ``default_vartype`` is ``None`` then missing
+                variables raise a ``ValueError``.
+            default_lower_bound: The lower bound of any variables not already
+                in the model. Ignored if ``default_vartype`` is ``None`` or
+                when the variable is :class:`~dimod.Vartype.BINARY` or
+                :class:`~dimod.Vartype.SPIN`.
+            default_upper_bound: The upper bound of any variables not already
+                in the model. Ignored if ``default_vartype`` is ``None`` or
+                when the variable is :class:`~dimod.Vartype.BINARY` or
+                :class:`~dimod.Vartype.SPIN`.
 
         Raises:
-            ValueError: If the variable is not in the model.
+            ValueError: If the variable is not in the model and
+            ``default_vartype`` is ``None``.
+
         """
         return self.data.add_linear
 
-    def add_linear_from(self, linear: Union[Mapping[Variable, Bias],
-                                            Iterable[Tuple[Variable, Bias]]]):
+    def add_linear_from(self,
+                        linear: Union[Mapping[Variable, Bias], Iterable[Tuple[Variable, Bias]]],
+                        *,
+                        default_vartype=None,
+                        default_lower_bound=None,
+                        default_upper_bound=None,
+                        ):
         """Add variables and linear biases to a quadratic model.
 
         Args:
@@ -420,18 +442,36 @@ class QuadraticModel(QuadraticViewsMixin):
                 form ``{v: bias, ...}`` or an iterable of ``(v, bias)`` pairs,
                 where ``v`` is a variable and ``bias`` is its associated linear
                 bias.
+            default_vartype: The vartype of any variables not already in the
+                model. If ``default_vartype`` is ``None`` then missing
+                variables raise a ``ValueError``.
+            default_lower_bound: The lower bound of any variables not already
+                in the model. Ignored if ``default_vartype`` is ``None`` or
+                when the variable is :class:`~dimod.Vartype.BINARY` or
+                :class:`~dimod.Vartype.SPIN`.
+            default_upper_bound: The upper bound of any variables not already
+                in the model. Ignored if ``default_vartype`` is ``None`` or
+                when the variable is :class:`~dimod.Vartype.BINARY` or
+                :class:`~dimod.Vartype.SPIN`.
 
         Raises:
-            ValueError: If a specified variable is not in the model.
+            ValueError: If the variable is not in the model and
+            ``default_vartype`` is ``None``.
+
         """
         add_linear = self.data.add_linear
 
+        if default_vartype is not None:
+            default_vartype = as_vartype(default_vartype, extended=True)
+
         if isinstance(linear, Mapping):
-            for v, bias in linear.items():
-                add_linear(v, bias)
-        else:
-            for v, bias in linear:
-                add_linear(v, bias)
+            linear = linear.items()
+
+        for v, bias in linear:
+            add_linear(v, bias,
+                       default_vartype=default_vartype,
+                       default_lower_bound=default_lower_bound,
+                       default_upper_bound=default_upper_bound)
 
     @forwarding_method
     def add_quadratic(self, u: Variable, v: Variable, bias: Bias):

dimod/quadratic/cyqm/cyqm_template.pyx.pxi
@@ -183,8 +183,24 @@ cdef class cyQM_template(cyQMBase):
         cdef bias_type *b = &(self.cppqm.linear(vi))
         b[0] = bias
 
-    def add_linear(self, v, bias_type bias):
-        cdef Py_ssize_t vi = self.variables.index(v)
+    def add_linear(self, v, bias_type bias, *,
+                   default_vartype=None,
+                   default_lower_bound=None,
+                   default_upper_bound=None,
+                   ):
+        cdef Py_ssize_t vi
+
+        if default_vartype is None or self.variables.count(v):
+            # already present
+            vi = self.variables.index(v)
+        else:
+            # we need to add it
+            vi = self.num_variables()
+            self.add_variable(default_vartype, v,
+                              lower_bound=default_lower_bound,
+                              upper_bound=default_upper_bound,
+                              )
+
         self._add_linear(vi, bias)
 
     def add_linear_from_array(self, ConstNumeric[:] linear):

releasenotes/notes/QM.add_linear-default_vartype-370a4876806affb0.yaml
@@ -1,0 +1,6 @@
+---
+features:
+  - |
+    Add the ability to specify a default variable type and bounds for missing
+    variables when calling ``QuadraticModel.add_linear()`` and
+    ``QuadraticModel.add_linear_from()``.

tests/test_quadratic_model.py
@@ -183,6 +183,33 @@ class TestAddVariablesFromModel(unittest.TestCase):
             self.assertEqual(new.upper_bound(v), qm.upper_bound(v))
 
 
+class TestAddLinear(unittest.TestCase):
+    def test_default_vartype(self):
+        qm = dimod.QuadraticModel()
+        qm.add_variable('INTEGER', 'i')
+        qm.add_variable('BINARY', 'x')
+
+        qm.add_linear_from({'i': 1, 'x': 2, 'y': 3, 'z': 4}, default_vartype='BINARY')
+
+        self.assertEqual(qm.linear, {'i': 1, 'x': 2, 'y': 3, 'z': 4})
+        self.assertEqual(qm.vartype('i'), dimod.INTEGER)
+        for v in 'xyz':
+            self.assertEqual(qm.vartype(v), dimod.BINARY)
+
+        qm.add_linear_from({'x': 3, 'j': 1}, default_vartype='INTEGER',
+                           default_lower_bound=-2, default_upper_bound=7)
+
+        self.assertEqual(qm.linear['x'], 5)
+        self.assertEqual(qm.vartype('j'), dimod.INTEGER)
+        self.assertEqual(qm.lower_bound('j'), -2)
+        self.assertEqual(qm.upper_bound('j'), 7)
+
+    def test_missing(self):
+        qm = dimod.QuadraticModel()
+        with self.assertRaises(ValueError):
+            qm.add_linear('a', 1)
+
+
 class TestAddQuadratic(unittest.TestCase):
     def test_self_loop_spin(self):
         qm = QM(vartypes={'i': 'INTEGER', 's': 'SPIN', 'x': 'BINARY'})

commit 43e62d48f8170702771e35b1a439fbcb6d20d35b

Speed up QM.add_linear_from()
77fd6af995c2b940dedc4975d320c690aa019b31
(see https://github.com/dwavesystems/dimod/pull/1170) introduced a
performance regression. This commit fixes it.

dimod/quadratic/quadratic_model.py
@@ -462,17 +462,38 @@ class QuadraticModel(QuadraticViewsMixin):
         """
         add_linear = self.data.add_linear
 
-        if default_vartype is not None:
-            default_vartype = as_vartype(default_vartype, extended=True)
-
         if isinstance(linear, Mapping):
             linear = linear.items()
 
-        for v, bias in linear:
-            add_linear(v, bias,
-                       default_vartype=default_vartype,
-                       default_lower_bound=default_lower_bound,
-                       default_upper_bound=default_upper_bound)
+        # checking whether the keyword arguments are present actually
+        # results in a pretty shocking performance difference, almost x2
+        # for when they are not there
+        # I did try using functools.partial() as well
+        if default_vartype is None:
+            if default_lower_bound is None and default_upper_bound is None:
+                for v, bias in linear:
+                    add_linear(v, bias)
+            else:
+                for v, bias in linear:
+                    add_linear(v, bias,
+                               default_lower_bound=default_lower_bound,
+                               default_upper_bound=default_upper_bound,
+                               )
+        else:
+            default_vartype = as_vartype(default_vartype, extended=True)
+
+            if default_lower_bound is None and default_upper_bound is None:
+                for v, bias in linear:
+                    add_linear(v, bias,
+                               default_vartype=default_vartype,
+                               )
+            else:
+                for v, bias in linear:
+                    add_linear(v, bias,
+                               default_vartype=default_vartype,
+                               default_lower_bound=default_lower_bound,
+                               default_upper_bound=default_upper_bound,
+                               )
 
     @forwarding_method
     def add_quadratic(self, u: Variable, v: Variable, bias: Bias):

releasenotes/notes/fix-qm.add_linear-performance-regression-8d97458c4a494240.yaml
@@ -1,0 +1,7 @@
+---
+fixes:
+  - |
+    Fix a performance regression to ``QuadraticModel.add_linear_from()`` introduced by
+    `#1170 <https://github.com/dwavesystems/dimod/pull/1170>`_ as part of
+    the `0.11.0 <https://github.com/dwavesystems/dimod/releases/tag/0.11.0>`_
+    release.