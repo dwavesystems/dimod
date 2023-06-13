0943e7c9f1a80600e06db843b41e162b628d3cdb
Alexander Condello <arcondello@gmail.com>
2023-06-13 11:18:50 -0700

    Update version 0.12.7 -> 0.12.8rc0

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 
 # version is used by serialization below so we need it before everything
-__version__ = '0.12.7'
+__version__ = '0.12.8rc0'
 
 from dimod.constrained import *
 import dimod.constrained
