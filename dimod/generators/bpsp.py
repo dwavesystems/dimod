509e9847fc6d83e00486fcb8c6eb869135d28631
pfarre <pfarre@dwavesys.com>
2024-02-21 11:22:52 -0800

    Formatting. Add release notes

--- dimod/generators/bpsp.py
+++ dimod/generators/bpsp.py
@@ -12,75 +12,75 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import dimod
-import numpy.random as rnd
-
 __all__ = ["random_binary_paint_shop_problem"]
 
+import typing
+import numpy.random
 
-def binary_paint_shop_problem(car_sequence):
-    """
+import dimod
+
+
+def binary_paint_shop_problem(car_sequence: typing.Sequence[typing.Hashable]) -> dimod.BinaryQuadraticModel:
+    """Generates a binary quadratic model representing a binary paint shop problem.
+
     Ising formulation for Binary Paint Shop Problem:
     - Streif, Michael, et al.
       "Beating classical heuristics for the binary paint shop
       problem with the quantum approximate optimization algorithm."
       Physical Review A 104.1 (2021): 012403.
 
-    Input:
-    - car_sequence: list of car labels
-    - seed: Seed for the random number generator
+    Args:
+        car_sequence: A list of car labels.
 
     Returns:
-    - BQM: Ising model where each variable is a car, and the value indicates what color it gets painted first
+        Ising model where each variable is a car, and the value indicates what color it gets painted first.
     """
 
     car_counter = {car: 0 for car in car_sequence}
-    assert len(car_sequence) == 2 * len(car_counter)
+    if len(car_sequence) != 2 * len(car_counter):
+        raise ValueError("Car labels are not unique")
 
-    bqm = dimod.BQM(vartype=dimod.SPIN)
+    bqm = dimod.BinaryQuadraticModel(vartype=dimod.SPIN)
     for car1, car2 in zip(car_sequence, car_sequence[1:]):
-        bqm += (
-            (-1) ** (car_counter[car1] + car_counter[car2] + 1)
-            * dimod.Spin(car1)
-            * dimod.Spin(car2)
-        )
+        if car1 != car2:
+            bqm.add_quadratic(car1, car2, (-1) ** (car_counter[car1] + car_counter[car2] + 1))
         car_counter[car1] += 1
-
     return bqm
 
 
-def random_binary_paint_shop_problem(n_cars, seed=None):
-    """
-    Generates a random binary paint shop problem
+def random_binary_paint_shop_problem(n_cars: int, seed: int = None) -> dimod.BinaryQuadraticModel:
+    """Generates a random binary paint shop problem.
 
     Binary Paint Shop Problem:
     - Epping, Th, Winfried Hochstättler, and Peter Oertel.
       "Complexity results on a paint shop problem."
       Discrete Applied Mathematics 136.2-3 (2004): 217-226.
 
-    Input:
+    Args:
+        n_cars: Number of cars.
+        seed: Seed for the random number generator.
 
-    - n_cars (int): Number of cars
-    - seed: Seed for the random number generator
+    Returns:
+        Ising model for the random binary paint shop problem.
     """
 
-    rnd.seed(seed)
+    rng = numpy.random.RandomState(seed)
     car_sequence = list(range(n_cars)) * 2
-    rnd.shuffle(car_sequence)
+    rng.shuffle(car_sequence)
 
     return binary_paint_shop_problem(car_sequence)
 
 
-def sample_to_coloring(sample, car_sequence):
-    """Given a bqm sample, color the car sequence and return BPSP objective
+def sample_to_coloring(sample: typing.Dict, car_sequence: typing.Sequence[typing.Hashable]) -> typing.Tuple[typing.List, int]:
+    """Colors the car sequence based on a bqm sample and returns BPSP objective.
     
-    Input:
-        - sample: solution to `binary_paint_shop_problem` mapping variables to color
-        - car_sequence: sequence of appearence of cars
+    Args:
+        sample: Solution to `binary_paint_shop_problem` mapping variables to color.
+        car_sequence: Sequence of appearence of cars.
 
     Returns:
-        - car_colors: colored sequence
-        - color_changes: number of color changes (objective of BPSP problem)
+        car_colors: Colored car sequence.
+        color_changes: Number of color changes (objective of BPSP problem).
     """
     visited_cars = set()
     car_colors = []
