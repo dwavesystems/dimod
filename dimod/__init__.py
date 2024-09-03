9b17267e2db2db0f0687e63741db05ed6ca6df14
Alexander Condello <arcondello@gmail.com>
2024-09-03 07:56:14 -0700

    Update version 0.12.16 -> 0.12.17

--- dimod/__init__.py
+++ dimod/__init__.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 
 # version is used by serialization below so we need it before everything
-__version__ = '0.12.16'
+__version__ = '0.12.17'
 
 from dimod.constrained import *
 import dimod.constrained
