b0cac2bef6e40f6864d73121e4f43f39342bdb1d
Alexander Condello <arcondello@gmail.com>
2025-08-06 11:02:18 -0700

    Fix broken linkcheck

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ linkcheck_retries = 2
 linkcheck_anchors = False
 linkcheck_ignore = [r'https://cloud.dwavesys.com/leap',  # redirects, many checks
                     r'https://epubs.siam.org',           # ignores robots since Feb 2023
+                    r'https://paws.princeton.edu/',      # also rejects robots
                     r'.clang-format',
                     r'setup.cfg',
                     ]  # dimod RTD currently not building on boost
