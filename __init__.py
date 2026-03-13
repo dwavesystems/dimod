7a29940d1e83886215fbd8e891e9fec7329db821
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2026-03-13 18:53:36 +0100

    Update package setup/metadata after code review

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018 D-Wave Systems Inc.
+# Copyright 2026 D-Wave
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
